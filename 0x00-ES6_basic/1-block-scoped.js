4e7487ada441d0c3f5d5d6b08495f3e1ad23247c
FourtyThree43 <shaqmwa@outlook.com>
2023-09-25 23:01:42 +0300

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,7 +1,6 @@
-// Description: Block scoped variables
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
     let task = true;
