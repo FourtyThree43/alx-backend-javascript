a805f73e87d0beda0308a2cd9ed69779f2d080c9
FourtyThree43 <shaqmwa@outlook.com>
2023-10-03 18:28:45 +0300

    REALLY FUCKING FIXED

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,14 @@
 function cleanSet(set, startString) {
-  if (typeof startString !== 'string' || startString === '') {
+  if (startString === '') {
     return '';
   }
 
   const result = [];
 
   for (const item of set) {
-    if (item.startsWith(startString)) {
+    if (typeof item === 'string' && item.startsWith(startString)) {
       result.push(item.slice(startString.length));
     }
-
   }
 
   return result.join('-');
