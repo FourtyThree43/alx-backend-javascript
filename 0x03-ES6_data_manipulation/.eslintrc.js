a4116eb93fffbdb8ebcc993253b1c5ac54d5c969
FourtyThree43 <shaqmwa@outlook.com>
2023-10-03 13:14:53 +0300

    Basic list of objects

--- 0x03-ES6_data_manipulation/.eslintrc.js
+++ 0x03-ES6_data_manipulation/.eslintrc.js
@@ -5,33 +5,33 @@ module.exports = {
     jest: true,
   },
   extends: [
-    "airbnb-base",
-    "plugin:jest/all",
+    'airbnb-base',
+    'plugin:jest/all',
   ],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["jest"],
+  plugins: ['jest'],
   rules: {
-    "max-classes-per-file": "off",
-    "no-underscore-dangle": "off",
-    "no-console": "off",
-    "no-shadow": "off",
-    "no-restricted-syntax": [
-      "error",
-      "LabeledStatement",
-      "WithStatement",
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
     ],
   },
   overrides: [
     {
-      files: ["*.js"],
-      excludedFiles: "babel.config.js",
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
     },
   ],
 };
