4bb443ebebe4d37cded37002124e3675dafa9d17
FourtyThree43 <shaqmwa@outlook.com>
2023-10-03 13:21:21 +0300

    More mapping

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+function getListStudentIds(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  return students.map((student) => student.id);
+}
+
+export default getListStudentIds;
